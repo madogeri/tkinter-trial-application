3bc7f1a71e707b4063efa53f5c660a6adb984f0c
madogeri <119082125+madogeri@users.noreply.github.com>
2025-08-30 03:37:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import os
-import sys
-
-
-
-print("testing program to ensure it is connected to my github")
-print("I received your confirmation")
-print("Thankyou, i am sending you another message letting you know that it is confirmed")
-print("got your confirmation again Mike, thankyou")
-'''
-TODO clear the file 
-
-'''
+print(Successfully Cleared)
