fb7eb7106075415748755e7f88b790bc83973054
Adogeri <madoger@entergy.com>
2025-08-30 03:13:02 -0500

    revised the message for confirmation

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ import sys
 
 print("testing program to ensure it is connected to my github")
 print("I received your confirmation")
+print("Thankyou, i am sending you another message letting you know that it is confirmed")
