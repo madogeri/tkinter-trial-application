db84e312ede8863112c49e3354d1254c7ccef7ef
Adogeri <madoger@entergy.com>
2025-08-30 03:35:28 -0500

    sending command

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ print("testing program to ensure it is connected to my github")
 print("I received your confirmation")
 print("Thankyou, i am sending you another message letting you know that it is confirmed")
 print("got your confirmation again Mike, thankyou")
+'''
+TODO clear the file 
+
+'''
